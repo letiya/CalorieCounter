1ae336f10f25ed3b2754e9a27ec07c1dbf09c137
Belle Lee <letiya66@gmail.com>
2018-09-25 23:55:06 -0700

    Add settings option in activity_pet_detail. Add tool bars and 'Next' buttons in some UIs. Replace two fragments with Activities.

---
+++ CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/PetDetailActivity.java
@@ -0,0 +1,48 @@
+package com.letiyaha.android.caloriecounter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PetDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_detail);
+
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.menu_pet_detail, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(startSettingsActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

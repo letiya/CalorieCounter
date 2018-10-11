36aa23ec2f463f1b997b72621d1104a9b44f5cc3
Belle Lee <letiya66@gmail.com>
2018-10-10 23:28:30 -0700

    Add adds to PetDetailActivity UI.

--- CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/PetDetailActivity.java
+++ CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/PetDetailActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.letiyaha.android.caloriecounter.Models.Database;
 import com.letiyaha.android.caloriecounter.Models.PetProfile;
 
@@ -36,6 +38,9 @@ public class PetDetailActivity extends AppCompatActivity {
 
     @BindView(R.id.bt_pet_health)
     Button mBtPetHealth;
+
+    @BindView(R.id.adView)
+    AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,6 +84,15 @@ public class PetDetailActivity extends AppCompatActivity {
                 startActivity(intentToStartMyHealthActivity);
             }
         });
+
+        // 4. Set up ads.
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
     }
 
     @Override

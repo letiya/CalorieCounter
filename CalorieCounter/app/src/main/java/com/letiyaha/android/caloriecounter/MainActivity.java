53f4359f7295d472461a4bdceb2588c3ae555bef
letiya <letiya66@gmail.com>
2018-10-31 07:14:46 -0700

    Moved content related to user profile in MainActivity to a new activity, UserActivity. Check if user profile was created or not in MainActivity. Go to PetDetailActivity if yes, go to UserActivity otherwise.

--- CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/MainActivity.java
+++ CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/MainActivity.java
@@ -4,79 +4,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.letiyaha.android.caloriecounter.Models.Database;
-import com.letiyaha.android.caloriecounter.Models.Util;
-import com.squareup.picasso.Picasso;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.letiyaha.android.caloriecounter.Models.PetProfile;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context mContext;
-
-    private static final String ICON_USER = "https://cdn.pixabay.com/photo/2016/04/26/12/25/male-1354358__480.png";
-//    private static final String ICON_USER = "https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579__480.png";
-
-    private static final String ICON_DOB = "https://cdn.pixabay.com/photo/2012/04/05/00/42/cake-25388__480.png";
-
-    @BindView(R.id.iv_user_name)
-    ImageView mIvUsername;
-
-    @BindView(R.id.iv_dob)
-    ImageView mIvDob;
-
-    @BindView(R.id.et_username)
-    EditText mEtUsername;
-
-    @BindView(R.id.et_dob)
-    EditText mEtDob;
-
-    @BindView(R.id.et_height)
-    EditText mEtHeight;
-
-    @BindView(R.id.et_weight)
-    EditText mEtWeight;
-
-    @BindView(R.id.bt_my_profile_next)
-    Button mBtNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-
         mContext = getApplicationContext();
 
-        Picasso.with(mContext).load(ICON_USER).into(mIvUsername);
-        Picasso.with(mContext).load(ICON_DOB).into(mIvDob);
+        Database db = Database.getInstance();
 
-        mBtNext.setOnClickListener(new View.OnClickListener() {
+        db.readPetProfile(new Database.ReadPetProfileCallback() {
             @Override
-            public void onClick(View v) {
-                if (mEtUsername.getText().toString().length() == 0 || mEtDob.getText().toString().length() == 0 || mEtHeight.getText().toString().length() == 0 || mEtWeight.getText().toString().length() == 0) {
-                    Toast.makeText(mContext, "Required field is blank!", Toast.LENGTH_SHORT).show();
-                } else if (!Util.isDateValid(mEtDob.getText().toString(), "yyyy/MM/dd")) {
-                    Toast.makeText(mContext, "Date of birth format is incorrect!", Toast.LENGTH_SHORT).show();
-                } else if (!Util.isNumber(mEtHeight.getText().toString())) {
-                    Toast.makeText(mContext, "Entered height is not a number!", Toast.LENGTH_SHORT).show();
-                } else if (!Util.isNumber(mEtWeight.getText().toString())) {
-                    Toast.makeText(mContext, "Entered weight is not a number!", Toast.LENGTH_SHORT).show();
+            public void onCallback(PetProfile petProfile) {
+                if (petProfile.getPetName().length() > 0) {
+                    // Go to PetDetailActivity.
+                    Intent intentToStartPetDetailActivity = new Intent(mContext, PetDetailActivity.class);
+                    startActivity(intentToStartPetDetailActivity);
                 } else {
-                    // 1. Save data to db
-                    Database db = Database.getInstance();
-                    db.insertProfile(mEtUsername.getText().toString(), mEtDob.getText().toString(), mEtHeight.getText().toString(), mEtWeight.getText().toString());
-                    // 2. Go to PetActivity.
-                    Intent intentToStartPetActivity = new Intent(mContext, PetActivity.class);
-                    startActivity(intentToStartPetActivity);
+                    // Go to UserActivity.
+                    Intent intentToStartUserActivity = new Intent(mContext, UserActivity.class);
+                    startActivity(intentToStartUserActivity);
                 }
             }
         });
---
+++ CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/UserActivity.java
@@ -0,0 +1,89 @@
+package com.letiyaha.android.caloriecounter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.letiyaha.android.caloriecounter.Models.Database;
+import com.letiyaha.android.caloriecounter.Models.Util;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class UserActivity extends AppCompatActivity {
+
+    private Context mContext;
+
+    private static final String ICON_USER = "https://cdn.pixabay.com/photo/2016/04/26/12/25/male-1354358__480.png";
+//    private static final String ICON_USER = "https://cdn.pixabay.com/photo/2016/04/15/18/05/computer-1331579__480.png";
+
+    private static final String ICON_DOB = "https://cdn.pixabay.com/photo/2012/04/05/00/42/cake-25388__480.png";
+
+    @BindView(R.id.iv_user_name)
+    ImageView mIvUsername;
+
+    @BindView(R.id.iv_dob)
+    ImageView mIvDob;
+
+    @BindView(R.id.et_username)
+    EditText mEtUsername;
+
+    @BindView(R.id.et_dob)
+    EditText mEtDob;
+
+    @BindView(R.id.et_height)
+    EditText mEtHeight;
+
+    @BindView(R.id.et_weight)
+    EditText mEtWeight;
+
+    @BindView(R.id.bt_my_profile_next)
+    Button mBtNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+
+        ButterKnife.bind(this);
+
+        mContext = getApplicationContext();
+
+        Picasso.with(mContext).load(ICON_USER).into(mIvUsername);
+        Picasso.with(mContext).load(ICON_DOB).into(mIvDob);
+
+        mBtNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEtUsername.getText().toString().length() == 0 || mEtDob.getText().toString().length() == 0 || mEtHeight.getText().toString().length() == 0 || mEtWeight.getText().toString().length() == 0) {
+                    Toast.makeText(mContext, "Required field is blank!", Toast.LENGTH_SHORT).show();
+                } else if (!Util.isDateValid(mEtDob.getText().toString(), "yyyy/MM/dd")) {
+                    Toast.makeText(mContext, "Date of birth format is incorrect!", Toast.LENGTH_SHORT).show();
+                } else if (!Util.isNumber(mEtHeight.getText().toString())) {
+                    Toast.makeText(mContext, "Entered height is not a number!", Toast.LENGTH_SHORT).show();
+                } else if (!Util.isNumber(mEtWeight.getText().toString())) {
+                    Toast.makeText(mContext, "Entered weight is not a number!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 1. Save data to db
+                    Database db = Database.getInstance();
+                    db.insertProfile(mEtUsername.getText().toString(), mEtDob.getText().toString(), mEtHeight.getText().toString(), mEtWeight.getText().toString());
+                    // 2. Go to PetActivity.
+                    Intent intentToStartPetActivity = new Intent(mContext, PetActivity.class);
+                    startActivity(intentToStartPetActivity);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(false);
+    }
+}

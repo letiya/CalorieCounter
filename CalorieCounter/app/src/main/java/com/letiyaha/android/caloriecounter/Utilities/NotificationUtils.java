32148fd6a6e16921b1d74e52395bc8ed9c6cbfe7
Belle Lee <letiya66@gmail.com>
2018-11-09 22:41:04 -0800

    Create a notification.

---
+++ CalorieCounter/app/src/main/java/com/letiyaha/android/caloriecounter/Utilities/NotificationUtils.java
@@ -0,0 +1,62 @@
+package com.letiyaha.android.caloriecounter.Utilities;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.letiyaha.android.caloriecounter.PetDetailActivity;
+import com.letiyaha.android.caloriecounter.R;
+
+public class NotificationUtils {
+
+    private static final int MEAL_REMINDER_NOTIFICATION_ID = 8102; // This notification ID can be used to access our notification after we've displayed it.
+    private static final int MEAL_REMINDER_PENDING_INTENT_ID = 2018; // This pending intent id is used to uniquely reference the pending intent
+    private static final String MEAL_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
+
+    public static void remindUserBecauseCharging(Context context) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel mChannel = new NotificationChannel(MEAL_REMINDER_NOTIFICATION_CHANNEL_ID, context.getString(R.string.main_notification_channel_name), NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, MEAL_REMINDER_NOTIFICATION_CHANNEL_ID)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle(context.getString(R.string.reminder_notification_title))
+                .setContentText(context.getString(R.string.reminder_notification_body))
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(
+                        context.getString(R.string.reminder_notification_body)))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+
+        notificationManager.notify(MEAL_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private static PendingIntent contentIntent(Context context) {
+        Intent startActivityIntent = new Intent(context, PetDetailActivity.class);
+        return PendingIntent.getActivity(context, MEAL_REMINDER_PENDING_INTENT_ID, startActivityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private static Bitmap largeIcon(Context context) {
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.meal_image);
+        return largeIcon;
+    }
+}
